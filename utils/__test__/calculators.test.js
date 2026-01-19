dc258a7f6f3da6c00ec7038d0fb4bba75593ccd4
Nauel Gómez <ngomez@codiara.com>
2026-01-19 21:28:21 +0100

    todo excepciones

--- utils/__test__/calculators.test.js
+++ utils/__test__/calculators.test.js
@@ -33,4 +33,22 @@ describe("calculatePriceWithDiscount", () => {
         ).toBe(0);
     });
 
+    it('Debe calcular descuentos de precios con decimales correctamente', () => {
+        const precioBase = 99.99;
+        const descuento = 10;
+        const resultado = calculatePriceWithDiscount(precioBase, descuento);
+        expect(resultado).toBe(89.99);
+        expect(resultado).toBeCloseTo(89.99, 6); // Nos permite trabajar con decimales por aproximación.
+    });
+
+    it('Debe lanzar una excepción si el precio es negativo', () => {
+        const precioBase = -10;
+        const descuento = 10;
+
+        expect(() => {
+            calculatePriceWithDiscount(precioBase, descuento)
+        }).toThrow('El precio base debe ser un número positivo');
+    });
+
+    it.todo('Debe lanzar un error si el descuento es negativo');
 });
