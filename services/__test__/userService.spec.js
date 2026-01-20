7560e7dba9be1a13baeb07e8f5b1f87f31dfe780
Nauel Gómez <ngomez@codiara.com>
2026-01-20 19:56:00 +0100

    mocks 1 done

--- services/__test__/userService.spec.js
+++ services/__test__/userService.spec.js
@@ -1,5 +1,29 @@
-import { createUser } from '../userService';
+import { jest } from '@jest/globals'; 
 
+import { createUser } from '../userService';
+import User from '../../models/User.js';
+
+jest.mock('../../models/User.js');
+User.findOne = jest.fn();
+User.findById = jest.fn();
+User.hashPassword = jest.fn();
+User.save = jest.fn();
+User.defaut = () => mockInstanceUser;
+
+const mockInstanceUser = {
+    save: jest.fn(),
+}
+
+jest.mock('../../utils/formatters.js', () => ({
+    formatEmail: mockFormatEmail,
+}));
+const mockFormatEmail = jest.fn();
+
+// jest.mock('../../utils/formatters.js', () => {
+//     return {
+//         formatEmail: jest.fn(),
+//     }
+// })
 
 describe('createUser', () => {
 
@@ -14,5 +38,54 @@ describe('createUser', () => {
         await expect(createUser(userData)).rejects.toThrow('El email no tiene un formato válido');
     });
 
-    it.todo('Debe lanzar error si la contraseña no es válida');
+    it('Debe lanzar error si la contraseña no es válida', async () => {
+        const userData = {
+            email: 'email@example.com',
+            password: '123'
+        };
+
+        expect.assertions(1);
+        await expect(createUser(userData))
+            .rejects.toThrow('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número');
+    });
+
+    // Mocks
+    it('Debe lanzar un error si el usuario ya existe', async () => {
+        const userData = {
+            email: 'test@ejemplo.com',
+            password: 'Password1234'
+        };
+
+        const existingUser = {
+            email: 'existing@ejemplo.com'
+        };
+
+        User.findOne.mockResolvedValueOnce(existingUser);
+        const spy = jest.spyOn(User, 'findOne');
+
+        await expect(createUser(userData))
+            .rejects.toThrow('Ya existe un usuario con ese email');
+        expect(spy).toHaveBeenCalled();
+        expect(spy).toHaveBeenCalledWith({ email: 'test@ejemplo.com' })
+
+    });
+
+    test.skip('debe llamar a formatEmail con el email proporcionado', async () => {
+        const userData = {
+            email: ' Test@Ejemplo.cOM',
+            password: 'Password1234'
+        }
+
+        // Se esta llamando correctamente formatEmail?
+        // const spy = jest.spyOn(createUser, 'formatEmail');
+
+        User.findOne.mockResolvedValueOnce(null);
+        User.hashPassword.mockResolvedValueOnce(userData.password);
+        mockInstanceUser.save.mockResolvedValueOnce({});
+
+        await createUser(userData);
+        expect(mockFormatEmail).toHaveBeenCalled();
+
+        // El resultado es correcto? test@ejemplo.com
+    }, 10000); // Timeout de 10s
 });
