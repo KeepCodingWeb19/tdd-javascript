d6c5c8fe31de19f5cd8c5042b58e87ac8026f3f3
Nauel Gómez <ngomez@codiara.com>
2026-01-20 20:25:10 +0100

    TDD 1: todo

---
+++ utils/__test__/phoneFormatter.test.js
@@ -0,0 +1,10 @@
+// Vamos a crear la función phoneFormatter siguiendo TDD
+
+// 1: Debe formatear un número de 9 dígitos sin prefijo (happy path)
+// Si recibe como parámetro de entrada 612345678 debe devover +34 612 345 678
+
+
+// 2: Debe formatear un número que ya tiene prefijo +34
+// Si recibe +34612345678 debe devolver +34 612 345 678
+
+// 3: Debe eliminar espacios del número de entrada ' 612345678' debe devolver +34 612 345 678
