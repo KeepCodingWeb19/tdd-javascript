6ac86426e1f72c23accef5db544462f372b49d47
Nauel Gómez <ngomez@codiara.com>
2026-01-22 19:06:40 +0100

    Tdd OK

--- utils/__test__/phoneFormatter.test.js
+++ utils/__test__/phoneFormatter.test.js
@@ -50,4 +50,58 @@ describe('formatPhoneNumber', () => {
         ).toBe('+34 612 345 678');
     });
 
+    // 5. TEST 5: Validación - número demasiado corto
+    // 61234567 -> Debe devolver una excepción con un número de 8 digitos.
+    it('Debe lanzar una excepción si el numero tiene menos de 9 dígitos', () => {
+        const phoneNumber = '61234567';
+        expect(() => {
+            formatPhoneNumber(phoneNumber)
+        }).toThrow('El número de teléfono debe tener 9 dígitos');
+    });
+
+    /**
+   * TEST 6: Validación - número demasiado largo
+   */
+  test('debe lanzar error si el número tiene más de 9 dígitos (sin prefijo)', () => {
+    const phoneNumber = '61234567891';
+    expect(() => {
+        formatPhoneNumber(phoneNumber)
+    }).toThrow('El número de teléfono debe tener 9 dígitos');
+  });
+
+  // TEST 7: Debe poder admitir varios tipos de prefijos: +34, 0034 o 34
+  test('Debe poder admitir varios tipos de prefijos: +34, 0034 o 34', () => {
+    expect(
+        formatPhoneNumber('+34612345678')
+    ).toBe('+34 612 345 678')
+    expect(
+        formatPhoneNumber('0034612345678')
+    ).toBe('+34 612 345 678')
+    expect(
+        formatPhoneNumber('34612345678')
+    ).toBe('+34 612 345 678')
+  });
+
+  // Test 8: Debe validar correctamente los numeros reales
+  // En españa empiezan por 6, 7, 8, 9
+  it('Debe validar numeraciones españolas', () => {
+    expect( () => {
+        formatPhoneNumber('212345678')
+    }
+    ).toThrow('El número de telefono debe empezar por 6, 7, 8 o 9');
+    expect( () => {
+        formatPhoneNumber('312345678')
+    }
+    ).toThrow('El número de telefono debe empezar por 6, 7, 8 o 9');
+    expect( () => {
+        formatPhoneNumber('412345678')
+    }
+    ).toThrow('El número de telefono debe empezar por 6, 7, 8 o 9');
+    expect( () => {
+        formatPhoneNumber('512345678')
+    }
+    ).toThrow('El número de telefono debe empezar por 6, 7, 8 o 9');
+
+  });
+
 })
--- utils/phoneFormatter.js
+++ utils/phoneFormatter.js
@@ -2,9 +2,33 @@ export function formatPhoneNumber(phoneNumber) {
 
     let cleaned = phoneNumber.trim();
 
+
     cleaned = cleaned.replace(/[\s-()]/g, '');
     
-    cleaned = cleaned.replace('+34', '');
+    if (cleaned.startsWith('+34')) {
+        cleaned = cleaned.substring(3);
+    }
+
+    if (cleaned.startsWith('0034')) {
+        cleaned = cleaned.substring(4);
+    }
+
+    if (cleaned.startsWith('34') && cleaned.length > 9) {
+        cleaned = cleaned.substring(2);
+    }
+
+    if (cleaned.length !== 9) {
+        throw new Error('El número de teléfono debe tener 9 dígitos');
+    }
+
+    if (
+        !cleaned.startsWith(6) &&
+        !cleaned.startsWith(7) &&
+        !cleaned.startsWith(8) &&
+        !cleaned.startsWith(9)
+    ) {
+        throw new Error('El número de telefono debe empezar por 6, 7, 8 o 9');
+    }
 
     return `+34 ${cleaned.substring(0, 3)} ${cleaned.substring(3, 6)} ${cleaned.substring(6)}`;
 }
