ff22e702be81023db81a1170215a74cf2e7fe18e
Nauel Gómez <ngomez@codiara.com>
2026-01-22 20:01:21 +0100

    Int test: todo 1

---
+++ controllers/__test__/homeController.test.js
@@ -0,0 +1,43 @@
+import { jest } from '@jest/globals';
+
+import request from 'supertest';
+
+
+jest.unstable_mockModule('../../lib/sessionManager.js', () => {
+    return {
+        middleware: (req, res, next) => {
+            req.session = { userId: undefined };
+            // Imprescindible llamar a next() cuando mockeamos middlewares
+            // return res.status(201).json(); 
+            next();
+        },
+        useSessionInViews: (req, res, next) => {
+            res.locals.session = req.session || {};
+            next();
+        },
+        guard: (req, res, next) => next(),
+    }
+});
+
+// Import dinámico en ESM para poder mocker
+// const importApp = await import('../../app.js');
+// const app = importApp.default;
+const { default: app } = await import('../../app.js');
+
+// Test de integración.
+// App -> url sirve una ruta en concreto.
+// Middleware se aplica correctamente
+// Una petición contola correctamente sus errores
+
+describe('Test de Index', () => {
+
+    it("Debe devolver un 200 al visitar la home", async () => {
+        expect.assertions(1); 
+
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+
+    }, 15000);
+
+    it.todo("Debe devolver siempre un documento html al visitar la home");
+})
--- jest.config.mjs
+++ jest.config.mjs
@@ -2,4 +2,5 @@ export default {
     testEnvironment: 'node',
     transform: {},
     setupFilesAfterEnv: [],
+    clearMocks: true,
 }
